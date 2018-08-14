4c1e0fbe686892894eed68f81b9a51b04769d693
Antonia <antonia.ivanova09@yahoo.com>
2018-08-14 16:38:59 +0200

    Get avg stars for repositories

--- src/projectSelection/projectSelector.java
+++ src/projectSelection/projectSelector.java
@@ -1,20 +1,42 @@
 package projectSelection;
 
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.FuzzyQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.adjacency.AdjacencyMatrix.Bucket;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.*;
+import org.elasticsearch.search.aggregations.metrics.avg.Avg;
+import org.elasticsearch.search.aggregations.metrics.avg.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 
 import com.google.gson.JsonArray;
@@ -22,10 +44,14 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import utils.LevensteinDistanceImplementation;
+import vulnerabilitySearch.VulnerabilityDataToElasticImporter;
+
 public class projectSelector {
 
+	private RestHighLevelClient elasticClient;
 	private String OAuthToken = "183c10a9725ad6c00195df59c201040e1b3d1d07";
-	protected static String repositoryDatabaseName = "repositories_database";
+	protected static String repositoryDatabaseName = "repositories_database_no_minimum_stars";
 
 	class RepositoryResult {
 
@@ -76,7 +102,7 @@ public class projectSelector {
 		}
 	}
 
-	@Test
+	// @Test
 	public void projectSelector() {
 		searchForJavaRepositoryNames();
 	}
@@ -89,7 +115,7 @@ public class projectSelector {
 			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
 			// (requests x 100)
-			 for (int i = 1; i <= 100; i++) {
+			for (int i = 1; i <= 100; i++) {
 				url = "https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc"
 						+ "&access_token=" + OAuthToken + "&page=" + i + "&per_page=89";
 
@@ -109,7 +135,8 @@ public class projectSelector {
 					String fullName = jo.get("full_name").toString().replace("\"", "");
 					int stars = Integer.parseInt(jo.get("stargazers_count").toString());
 
-					if ((stars >= 100) && isGradleRepository(fullName)) {
+					// if ((stars >= 100) && isGradleRepository(fullName)) {
+					if (isGradleRepository(fullName)) {
 						System.out.println("MATCH : " + fullName);
 
 						String product = jo.get("name").toString().replace("\"", "");
@@ -183,8 +210,7 @@ public class projectSelector {
 			repositories.add(repository);
 		}
 
-		RestHighLevelClient client = new RestHighLevelClient(
-				RestClient.builder(new HttpHost("localhost", 9200, "http")));
+		elasticClient = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
 		IndexRequest indexRequest = null;
 
@@ -192,7 +218,7 @@ public class projectSelector {
 			indexRequest = new IndexRequest(repositoryDatabaseName, "doc").source(iterator.next());
 
 			try {
-				client.index(indexRequest);
+				elasticClient.index(indexRequest);
 			} catch (Exception e) {
 				System.err.println("Could not index document " + iterator.toString());
 				e.printStackTrace();
@@ -200,12 +226,38 @@ public class projectSelector {
 		}
 
 		try {
-			client.close();
+			elasticClient.close();
 		} catch (Exception e) {
 			System.err.println("Could not close RestHighLevelClient!");
 			e.printStackTrace();
 		}
 
 		System.out.println("Inserting " + repositories.size() + " documents into index.");
+	}
+
+	@Test
+	// private int getAverageNumberOfStars(){
+	public void getAverageNumberOfStars() {
+		elasticClient = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+		SearchRequest searchRequest = new SearchRequest(repositoryDatabaseName);
+		AvgAggregationBuilder avgAB = AggregationBuilders.avg("avg_stars").field("Stars");
+
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		searchSourceBuilder.query(QueryBuilders.matchAllQuery()).aggregation(avgAB);
+		searchRequest.source(searchSourceBuilder);
+
+		try {
+			SearchResponse searchResponse = elasticClient.search(searchRequest);
+			Aggregations aggregations = searchResponse.getAggregations();
+			
+			Avg avgStars = aggregations.get("avg_stars");
+	
+			double avg = avgStars.getValue();
+
+			System.out.println(avg);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
